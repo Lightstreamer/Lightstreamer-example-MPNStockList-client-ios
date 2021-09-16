5f3f8e2f63377a1ef5d3da876c1081cb0ce54010
acarioni <alessandro.carioni@lightstreamer.com>
2021-09-16 15:18:47 +0200

    convert NotificationController

--- StockWatch Extension/Utils.swift
+++ StockWatch Extension/Utils.swift
@@ -7,5 +7,9 @@
 //
 
 import Foundation
+import UIKit
 
 let FLASH_DURATION = 0.150
+
+let DARK_GREEN_COLOR = UIColor(red: 0.0000, green: 0.6000, blue: 0.2000, alpha: 1.0)
+let RED_COLOR = UIColor(red: 1.0000, green: 0.0706, blue: 0.0000, alpha: 1.0)
